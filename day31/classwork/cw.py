2c11deade832b5df5b5587527e29d4bc134ebb72
matesikharulidze <sikharulidzemate46@gmail.com>
2025-01-25 22:51:22 -0800

    commit

---
+++ day31/classwork/cw.py
@@ -0,0 +1,10 @@
+# ცვლადების შექმნა
+saxeli = "მათე"
+gvari = "სიხარულიძე"
+asaki = 13
+
+# ტექსტის ფორმატირება
+formatted_text = "ჩემი სახელია {}. ჩემი გვარია {} და ჩემი ასაკია {} წლის.".format(saxeli, gvari, asaki)
+
+# შედეგის გამოჩენა
+print(formatted_text)
